c7169b7a8ec6d10cb7bab961c2cd15ca83acbf86
Vignesh Nanda Kumar <vigneshnandakumar1997@gmail.com>
2022-11-14 11:24:38 -0800

    adding cursor border

--- app/Main.hs
+++ app/Main.hs
@@ -22,9 +22,25 @@ styleBlackCell = attrName "styleBlackCell"
 styleUnclickedCell = attrName "styleUnclickedCell"
 styleBoard = attrName "styleBoard"
 
+
+cursorBorderStyle :: BS.BorderStyle
+cursorBorderStyle =
+    BS.BorderStyle { BS.bsCornerTL = '/'
+                   , BS.bsCornerTR = '\\'
+                   , BS.bsCornerBR = '/'
+                   , BS.bsCornerBL = '\\'
+                   , BS.bsIntersectFull = '.'
+                   , BS.bsIntersectL = '.'
+                   , BS.bsIntersectR = '.'
+                   , BS.bsIntersectT = '.'
+                   , BS.bsIntersectB = '.'
+                   , BS.bsHorizontal = '*'
+                   , BS.bsVertical = '!'
+                   }
+
 attributes :: AttrMap
 attributes = attrMap V.defAttr
-  [ (stylePlayerCursor    , bg V.green)
+  [ (stylePlayerCursor    , fg V.green)
   , (styleRedCell , fg V.red)
   , (styleBlueCell , fg V.blue)
   , (styleYellowCell, fg V.yellow)
@@ -33,14 +49,42 @@ attributes = attrMap V.defAttr
   , (styleBoard, fg V.magenta)
   ]
 
+getColorBgStyle :: CardColor -> AttrName
+getColorBgStyle Red = styleRedCell
+getColorBgStyle Blue = styleBlueCell
+getColorBgStyle Black = styleBlackCell
+getColorBgStyle Yellow = styleYellowCell
+
+getClickedCursorStyle :: String -> CardColor -> Widget ()
+getClickedCursorStyle word color = withBorderStyle cursorBorderStyle $ B.border $ C.hCenter $ padAll 1 $ withAttr (getColorBgStyle color) $ str word
+
+getUnclickedCursorStyle :: String -> CardColor -> Widget ()
+getUnclickedCursorStyle word color = withBorderStyle cursorBorderStyle $ B.border $ C.hCenter $ padAll 1 $ withAttr styleUnclickedCell $ str word
+
+getClickedNormalStyle :: String -> CardColor -> Widget ()
+getClickedNormalStyle word color = withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ padAll 1 $ withAttr (getColorBgStyle color) $ str word
+
+
+getUnclickedNormalStyle ::  String -> CardColor -> Widget ()
+getUnclickedNormalStyle word color = withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ padAll 1 $ withAttr styleUnclickedCell $ str word
+
+drawPlayerCard :: PlayerCell -> Coord -> Widget ()
+drawPlayerCard (PCell (Loc cardx cardy) word True color) (Loc cursorx cursory) = if (and [(cursorx == cardx), (cursory == cardy)])
+                                                                                    then getClickedCursorStyle word color
+                                                                                    else getClickedNormalStyle word color
+                                                                                        
+drawPlayerCard (PCell (Loc cardx cardy) word False color) (Loc cursorx cursory) = if (and [(cursorx == cardx), (cursory == cardy)])
+                                                                                    then getUnclickedCursorStyle word color
+                                                                                    else getUnclickedNormalStyle word color
 
 drawGrid :: PlayerBoard -> Widget ()
 drawGrid pb = withBorderStyle BS.unicodeBold
     $ B.borderWithLabel ((withAttr styleBoard) $ str "Codenames Player View")
     $ vBox rows
     where
+        currCursor = cursor pb
         rows = [hBox $ (cardsInRow r) | r <- (plgrid pb)]
-        cardsInRow row = [hLimit 20 $ withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ padAll 1 $ (withAttr styleUnclickedCell $ str w) | (PCell _ w _ _) <- row]
+        cardsInRow row = [hLimit 20 $ (drawPlayerCard pcard currCursor) | pcard <- row]
 
 drawUI :: PlayerBoard -> Widget ()
 drawUI pb = drawGrid pb
@@ -53,6 +97,15 @@ wordList = ["COLD", "DEATH", "DIAMOND", "DOG", "DRESS", "FRANCE", "FIRE", "GLOVE
 pb :: PlayerBoard
 pb = createPlayerGrid wordList downloadedColorList
 
+g :: PlayerGrid
+g = [[PCell (Loc 0 0) "COLD" True Red,PCell (Loc 0 1) "DEATH" True Red,PCell (Loc 0 2) "DIAMOND" True Red,PCell (Loc 0 3) "DOG" False Red,PCell (Loc 0 4) "DRESS" False Red],[PCell (Loc 1 0) "FRANCE" False Red,PCell (Loc 1 1) "FIRE" False Red,PCell (Loc 1 2) "GLOVE" False Red,PCell (Loc 1 3) "GOLD" True Blue,PCell (Loc 1 4) "HAND" False Blue],[PCell (Loc 2 0) "JACK" False Blue,PCell (Loc 2 1) "LONDON" False Blue,PCell (Loc 2 2) "NEW YORK" True Blue,PCell (Loc 2 3) "SNOW" False Blue,PCell (Loc 2 4) "WATCH" False Blue],[PCell (Loc 3 0) "ALASKA" False Blue,PCell (Loc 3 1) "FROG" False Blue,PCell (Loc 3 2) "FROST" False Black,PCell (Loc 3 3) "CHAIN" False Yellow,PCell (Loc 3 4) "CHRISTMAS" False Yellow],[PCell (Loc 4 0) "COMB" False Yellow,PCell (Loc 4 1) "JEWELER" False Yellow,PCell (Loc 4 2) "HAIR" False Yellow,PCell (Loc 4 3) "LOVE" False Yellow,PCell (Loc 4 4) "STORY" False Yellow]]
+pb1 = PlayBoard 
+        {
+            cursor = (Loc 2 2),
+            plgrid = g
+        }
 
 main :: IO ()
-main = M.simpleMain (drawUI pb)
+main = M.simpleMain (drawUI pb1)
+
+
--- src/Game.hs
+++ src/Game.hs
@@ -5,6 +5,7 @@ module Game
   , PlayerBoard(..)
   , Direction(..)
   , CardColor(..)
+  , Coord(..)
   , createPlayerGrid
 
 ) where
