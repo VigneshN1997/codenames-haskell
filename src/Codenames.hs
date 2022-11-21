7096c0aadfc4d0da9001065c0b3394b949168589
Simran Barnwal <simranbarnwal@Simrans-MacBook-Air.local>
2022-11-21 11:40:04 -0800

    basic player game state

--- src/Codenames.hs
+++ src/Codenames.hs
@@ -8,7 +8,7 @@ import Game
 
 -- | The current status of the game
 data Codenames
-  = PlayerView PlayerBoard
+  = PlayerView PlayerGameState
 
 
 -- | Creates an initial 'Game' starting at the main menu
@@ -18,8 +18,8 @@ initialGame = PlayerView pb
 
 downloadedColorList :: [CardColor]
 downloadedColorList = [Red, Red, Red, Red, Red, Red, Red, Red, Blue, Blue, Blue, Blue, Blue, Blue, Blue, Blue, Blue, Black, Yellow, Yellow, Yellow, Yellow, Yellow, Yellow, Yellow]
-wordList :: [String]
-wordList = ["COLD", "DEATH", "DIAMOND", "DOG", "DRESS", "FRANCE", "FIRE", "GLOVE", "GOLD", "HAND", "JACK", "LONDON", "NEW YORK", "SNOW", "WATCH", "ALASKA", "FROG", "FROST", "CHAIN", "CHRISTMAS", "COMB", "JEWELER", "HAIR", "LOVE", "STORY"]
+egwordList :: [String]
+egwordList = ["COLD", "DEATH", "DIAMOND", "DOG", "DRESS", "FRANCE", "FIRE", "GLOVE", "GOLD", "HAND", "JACK", "LONDON", "NEW YORK", "SNOW", "WATCH", "ALASKA", "FROG", "FROST", "CHAIN", "CHRISTMAS", "COMB", "JEWELER", "HAIR", "LOVE", "STORY"]
 
-pb :: PlayerBoard
-pb = createPlayerGrid wordList downloadedColorList
+pb :: PlayerGameState
+pb = createPlayerGrid egwordList downloadedColorList
--- src/UI/PlayerBoard.hs
+++ src/UI/PlayerBoard.hs
@@ -50,13 +50,13 @@ drawPlayerCard (PCell (Loc cardx cardy) word False color) (Loc cursorx cursory) 
                                                                                     then getUnclickedCursorStyle word color
                                                                                     else getUnclickedNormalStyle word color
 
-drawGrid :: PlayerBoard -> Widget ()
+drawGrid :: PlayerGameState -> Widget ()
 drawGrid pb = withBorderStyle BS.unicodeBold
     $ B.borderWithLabel ((withAttr styleBoard) $ str "Codenames Player View")
     $ vBox rows
     where
-        currCursor = cursor pb
-        rows = [hBox $ (cardsInRow r) | r <- (plgrid pb)]
+        currCursor = playerCursor pb
+        rows = [hBox $ (cardsInRow r) | r <- (playerGrid pb)]
         cardsInRow row = [vLimit 30 $ hLimit 25 $ (drawPlayerCard pcard currCursor) | pcard <- row]
 
 
@@ -66,28 +66,27 @@ renderHint hintW hintNumW = vLimit 10 $ hLimit 30 $ withBorderStyle BS.unicodeBo
 getRedTeamScoreBoard :: Int -> Widget ()
 getRedTeamScoreBoard score = vLimit 10 $ hLimit 30 $ withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ withAttr styleRedCell $ str ("Team Red score :" ++ (show score))
 
-
 getBlueTeamScoreBoard :: Int -> Widget ()
 getBlueTeamScoreBoard score = vLimit 10 $ hLimit 30 $ withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ withAttr styleBlueCell $ str ("Team Blue score :" ++ (show score))
 
 renderPlayerTurn :: CardColor -> Widget ()
 renderPlayerTurn playerColor = vLimit 10 $ hLimit 30 $ withBorderStyle BS.unicodeBold $ B.border $ C.hCenter $ (withAttr (getColorBgStyle playerColor)) $ str ((show playerColor) ++ " Team's Turn")
 
-drawPlayerStats :: PlayerBoard -> Widget ()
-drawPlayerStats pb = ((getBlueTeamScoreBoard 4) <=> (getRedTeamScoreBoard 5)) <+> ((padLeft Max (renderHint "Hint" 3)) <=> (padLeft Max (renderPlayerTurn Blue)))
+drawPlayerStats :: PlayerGameState -> Widget ()
+drawPlayerStats pb = ((getBlueTeamScoreBoard (blueTeamScore pb)) <=> (getRedTeamScoreBoard (redTeamScore pb))) <+> ((padLeft Max (renderHint hintWord hintNum)) <=> (padLeft Max (renderPlayerTurn (teamTurn pb))))
+    where SHint hintWord hintNum = spyHint pb
 
-
-drawPlayerBoard :: PlayerBoard -> [Widget ()]
+drawPlayerBoard :: PlayerGameState -> [Widget ()]
 drawPlayerBoard pb = [(drawGrid pb) <=> (drawPlayerStats pb)]
 
 
-g :: PlayerGrid
-g = [[PCell (Loc 0 0) "COLD" True Red,PCell (Loc 0 1) "DEATH" True Red,PCell (Loc 0 2) "DIAMOND" True Red,PCell (Loc 0 3) "DOG" False Red,PCell (Loc 0 4) "DRESS" False Red],[PCell (Loc 1 0) "FRANCE" False Red,PCell (Loc 1 1) "FIRE" False Red,PCell (Loc 1 2) "GLOVE" False Red,PCell (Loc 1 3) "GOLD" True Blue,PCell (Loc 1 4) "HAND" False Blue],[PCell (Loc 2 0) "JACK" False Blue,PCell (Loc 2 1) "LONDON" False Blue,PCell (Loc 2 2) "NEW YORK" True Blue,PCell (Loc 2 3) "SNOW" False Blue,PCell (Loc 2 4) "WATCH" False Blue],[PCell (Loc 3 0) "ALASKA" False Blue,PCell (Loc 3 1) "FROG" False Blue,PCell (Loc 3 2) "FROST" False Black,PCell (Loc 3 3) "CHAIN" False Yellow,PCell (Loc 3 4) "CHRISTMAS" False Yellow],[PCell (Loc 4 0) "COMB" False Yellow,PCell (Loc 4 1) "JEWELER" False Yellow,PCell (Loc 4 2) "HAIR" False Yellow,PCell (Loc 4 3) "LOVE" False Yellow,PCell (Loc 4 4) "STORY" False Yellow]]
-pb1 = PlayBoard 
-        {
-            cursor = (Loc 2 2),
-            plgrid = g
-        }
+-- g :: PlayerGrid
+-- g = [[PCell (Loc 0 0) "COLD" True Red,PCell (Loc 0 1) "DEATH" True Red,PCell (Loc 0 2) "DIAMOND" True Red,PCell (Loc 0 3) "DOG" False Red,PCell (Loc 0 4) "DRESS" False Red],[PCell (Loc 1 0) "FRANCE" False Red,PCell (Loc 1 1) "FIRE" False Red,PCell (Loc 1 2) "GLOVE" False Red,PCell (Loc 1 3) "GOLD" True Blue,PCell (Loc 1 4) "HAND" False Blue],[PCell (Loc 2 0) "JACK" False Blue,PCell (Loc 2 1) "LONDON" False Blue,PCell (Loc 2 2) "NEW YORK" True Blue,PCell (Loc 2 3) "SNOW" False Blue,PCell (Loc 2 4) "WATCH" False Blue],[PCell (Loc 3 0) "ALASKA" False Blue,PCell (Loc 3 1) "FROG" False Blue,PCell (Loc 3 2) "FROST" False Black,PCell (Loc 3 3) "CHAIN" False Yellow,PCell (Loc 3 4) "CHRISTMAS" False Yellow],[PCell (Loc 4 0) "COMB" False Yellow,PCell (Loc 4 1) "JEWELER" False Yellow,PCell (Loc 4 2) "HAIR" False Yellow,PCell (Loc 4 3) "LOVE" False Yellow,PCell (Loc 4 4) "STORY" False Yellow]]
+-- pb1 = PlayBoard 
+--         {
+--             cursor = (Loc 2 2),
+--             plgrid = g
+--         }
 
 handleEvent (PlayerView pb) (VtyEvent (V.EvKey key [])) =
   continue $ case key of
@@ -95,7 +94,7 @@ handleEvent (PlayerView pb) (VtyEvent (V.EvKey key [])) =
     V.KDown  -> PlayerView (moveCursor DownD pb)
     V.KLeft  -> PlayerView (moveCursor LeftD pb)
     V.KRight -> PlayerView (moveCursor RightD pb)
-    V.KEnter -> PlayerView (selectCard pb)
+    V.KEnter -> PlayerView (updateCurrentTurnsAndScore (selectCard pb))
     _        -> PlayerView pb
 
 -- handleEvent :: PlayerBoard -> BrickEvent () e -> EventM () (Next PlayerBoard)
